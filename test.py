c528951e53c8454a22ba956c049e5da272727093
Cindy Weng <weng.cindy@gmail.com>
2019-11-02 13:37:04 -0400

    revised query

--- test.py
+++ test.py
@@ -5,8 +5,6 @@ import os
 from py2neo import Graph, Node, Relationship
 import pandas as pd
 
-from flask import Flask, request, jsonify
-app = Flask(__name__)
 
 graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
 graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
@@ -14,26 +12,16 @@ graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
 graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
 
 
-def get_email(participant):
+def serialize_genre(participant):
     return {
         'id': participant['id'],
         'email': participant['email'],
     }
 
-@app.route("/")
 
-def hello():
-	query = "MATCH (participant:Participant) RETURN participant limit 3"
-	result = graph.run(query)
+query = "MATCH (participant:Participant) RETURN participant limit 3"
+result = graph.run(query)
 
-	return [get_email(record['participant']) for record in result]
+print([serialize_genre(record['participant']) for record in result])
 
 
-
-@app.route("/test")
-def test():
-    return "meow"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
