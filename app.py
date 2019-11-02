8592914a7d5360283384cf61d6eef931338e79c5
Cindy Weng <weng.cindy@gmail.com>
2019-11-02 09:59:19 -0400

    initial graphdb commit

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 # app.py
+
+import os
+from neo4j.v1 import GraphDatabase, basic_auth
+
+from py2neo import Graph, authenticate
+
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
+graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
+graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
+
 
 @app.route("/")
 
 def hello():
-    return "Hello World!"
+    query = "CREATE (n:Person { name: 'Matt', title: 'Right' })"
+    graph.run(query)
+    query = "MATCH n RETURN n.name as name, n.title as title"
+	df = pd.DataFrame(graph.run(query))
+	df.columns=['name', 'title']
+
+    return df.head()
 
 
 @app.route("/test")
